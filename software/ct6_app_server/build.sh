96c576f58411d31795a3b1eea7e85fca6bc252c5
Paul Austen <pjaos@gmail.com>
2024-01-24 05:23:45 +0000

    Run pyflakes on all python apps

--- software/ct6_app_server/build.sh
+++ software/ct6_app_server/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Check the python files and exit on error.
-pyflakes3 ct6_db_store.py  ct6_tool.py
+pyflakes3 ct6_db_store.py ct6_dash.py ct6_dash_mgr.py ct6_tool.py ct6_mfg_tool.py
 
 sudo python3 -m pipenv2deb pipenv2deb
 
