b595db8df335e99e6bed21fe7aa9a9ac34708c14
Paul Austen <pjaos@gmail.com>
2024-05-06 10:25:31 +0100

    Remove the --three arg as its no longer needed

--- software/ct6_app_server/create_pip_env.sh
+++ software/ct6_app_server/create_pip_env.sh
@@ -3,4 +3,4 @@ export PIPENV_VENV_IN_PROJECT=enabled
 pipenv --rm
 # Create the .venv dir so that pipenv notices it's presence
 mkdir .venv
-python3 -m pipenv install --three
+python3 -m pipenv install
