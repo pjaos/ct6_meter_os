46f8d79126d206195f99ba15c249d97468e465f0
Paul Austen <pjaos@gmail.com>
2024-05-13 15:25:15 +0100

    Use the default location for the python env

--- software/ct6_app_server/windows_installer/create_pip_env.bat
+++ software/ct6_app_server/windows_installer/create_pip_env.bat
@@ -1,7 +1,7 @@
-SET PIPENV_VENV_IN_PROJECT=enabled
+REM SET PIPENV_VENV_IN_PROJECT=enabled
 python3 -m pipenv --rm
 REM Create the .venv dir so that pipenv notices it's presence
-mkdir .venv
+REM mkdir .venv
 python3 -m pipenv install --verbose
 REM Fixup error in the pyreadline module used by rshell
 python3 -m pipenv run pip uninstall -y pyreadline
