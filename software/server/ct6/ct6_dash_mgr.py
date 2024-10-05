ab04d2228ba9cea995db250f399a2ac8c159e8af
Paul Austen <pjaos@gmail.com>
2024-10-05 10:24:34 +0100

    Fix issue when running as root user under systemd where the bokeh_auth_attr_xxxxxx.json file can contain a /home/root/ path rather then /root/ which caused ct6_dash to fail to startup under systemd control.

--- software/server/ct6/ct6_dash_mgr.py
+++ software/server/ct6/ct6_dash_mgr.py
@@ -4,7 +4,10 @@ import  os
 from    p3lib.uio import UIO
 from    p3lib.bokeh_auth import CredentialsManager
 
-CRED_JSON_FILE = os.path.join( os. path. expanduser("~"), ".ct6_dash_credentials.json")
+CRED_JSON_FILE = os.path.join( os.path.expanduser("~"), ".ct6_dash_credentials.json")
+# If root user then the above can return an incorrect path (/home/root/...). Fix this.
+if os.geteuid()  == 0:
+    CRED_JSON_FILE = os.path.join( '/root', ".ct6_dash_credentials.json")
 
 def main():
     uio = UIO()
