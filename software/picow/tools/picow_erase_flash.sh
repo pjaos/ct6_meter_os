59d630351c95c7d7769d416ed2504ce415dbd2ec
Paul Austen <pjaos@gmail.com>
2025-02-06 23:08:06 +0000

    Add a link (software/picow -> software/server/picow) so that we don't keep the picow code in two places and can still build the windows installer.

--- software/picow/tools/picow_erase_flash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# This script requires one argument that contains the RPI-RP2
-# folder when the RPi PicoW is powered up with it's button 
-# being held down.
-cp picow_flash_images/flash_nuke.uf2 $1
