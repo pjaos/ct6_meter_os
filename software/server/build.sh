80f6a238db2bbf18485c667af2181f910a5d34f1
Paul Austen <pjaos@gmail.com>
2025-02-20 15:38:13 +0000

    Return the creation of the guit_hash.txt file when building the installer.

--- software/server/build.sh
+++ software/server/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
+
+# Create the git hash to be included in the installer file
+# for version tracking purposes.
+git rev-parse --short HEAD > assets/git_hash.txt
+
 rm -rf dist/ct6-*-py3-none-any.whl
 rm -rf installers/ct6-*-py3-none-any.whl
 rm -rf dist
