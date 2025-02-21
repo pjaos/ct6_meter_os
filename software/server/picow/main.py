7ce4dd219e450ab9db7bdc85d23cc7f5c0d5a669
Paul Austen <pjaos@gmail.com>
2025-02-21 15:17:12 +0000

    Update coment to use ct6_configurator.py tool.

--- software/server/picow/main.py
+++ software/server/picow/main.py
@@ -9,7 +9,8 @@ ALLOW_APP_TO_CRASH = True          # !!! This must be set to False on released p
                                    # This should be set to False on production code.
                                    # Note !!!
                                    # The main.py file is not updated when an over the air upgrade
-                                   # is performed. To update main.py use ct6_mfg_tool.py.
+                                   # is performed. To update main.py use ct6_configurator.py and
+                                   # Install the SW rather thyan upgrading it.
 def getActiveApp():
     """@brief Get the active app from the field in the config dict file.
        @return Either 1 or 2"""
