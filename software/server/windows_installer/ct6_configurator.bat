629a24a5858f888f992818243185d5def446d193
Paul Austen <pjaos@gmail.com>
2025-02-23 14:32:06 +0000

    Add messages to the cmd shell windows detailing how the user can shut down the GUI and the server.

--- software/server/windows_installer/ct6_configurator.bat
+++ software/server/windows_installer/ct6_configurator.bat
@@ -1,1 +1,3 @@
+REM Launching the application. It may take several seconds for the application to start up.
+REM To shutdown the application close the browser window (select X in corner), then close this window (select X in corner).
 python -m poetry run python -c "import ct6.ct6_configurator; ct6.ct6_configurator.main()" %*
