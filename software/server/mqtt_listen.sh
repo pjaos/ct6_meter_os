1f2ed195335f2ea43fc1282a322d918e5b0b31bb
Paul Austen <pjaos@gmail.com>
2025-02-28 13:27:48 +0000

    Update to pipe JSON data through jq to check the format of the data from the the MQTT server.

--- software/server/mqtt_listen.sh
+++ software/server/mqtt_listen.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This requires that the docker mosquito MQTT server is running
 # An example of subscribing to the CT6 topic. Used for testing
-# the CT6 MQTT server support.
-mosquitto_sub -v -t 'CT6'
+# the CT6 MQTT server support. This example puts the JSON message 
+# through the command line jq tool that checks the JSON syntax.
+mosquitto_sub -v -t 'CT6' -F "%p" | jq
