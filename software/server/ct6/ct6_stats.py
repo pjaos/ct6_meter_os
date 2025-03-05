67b04a812efe0102b2b949d436cc35478474db15
Paul Austen <pjaos@gmail.com>
2025-03-05 09:47:21 +0000

    Add a command line tool that will find all CT6 units on the LAN and return the JSON stats.
    This tool was designed to be a simple tool to discover all available
    CT6 stats.

---
+++ software/server/ct6/ct6_stats.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import argparse
+from p3lib.uio import UIO
+from p3lib.helper import logTraceBack
+from lib.yview import YViewCollector, LocalYViewCollector
+from time import sleep
+import rich
+import json
+
+class CT6Stats(object):
+    """@brief Responsible for discovering all CT6 units on the local network and displaying the stats as
+              JSON text on the command line."""
+    def __init__(self, uio, options):
+        """@brief Constructor
+           @param uio A UIO instance handling user input and output (E.G stdin/stdout or a GUI)
+           @param options An instance of the OptionParser command line options."""
+        self._uio = uio
+        self._options = options
+
+    def process(self):
+        """@brief Search for all CT6 units on the LAN and display stats received from all units."""
+        # Start running the local collector in a separate thread
+        self._localYViewCollector = LocalYViewCollector(self._uio, self._options)
+        self._localYViewCollector.setValidProuctIDList(YViewCollector.VALID_PRODUCT_ID_LIST)
+        self._localYViewCollector.addDevListener(self)
+        self._localYViewCollector.start()
+        # Wait here while until CTRL C
+        while True:
+            sleep(1)
+
+    def hear(self, devDict):
+        """@brief Called when data is received from the device.
+           @param devDict The device dict."""
+        rich.print_json(json.dumps(devDict))
+
+def main():
+    """@brief Program entry point"""
+    uio = UIO()
+
+    try:
+        parser = argparse.ArgumentParser(description="A tool to discover all CT6 units on the local network and displaying the stats as JSON text on the command line.",
+                                         formatter_class=argparse.RawDescriptionHelpFormatter)
+        parser.add_argument("-d", "--debug",  action='store_true', help="Enable debugging.")
+
+        options = parser.parse_args()
+
+        uio.enableDebug(options.debug)
+        ct6Stats = CT6Stats(uio, options)
+        ct6Stats.process()
+
+    # If the program throws a system exit exception
+    except SystemExit:
+        pass
+    # Don't print error information if CTRL C pressed
+    except KeyboardInterrupt:
+        pass
+    except Exception as ex:
+        logTraceBack(uio)
+
+        if options.debug:
+            raise
+        else:
+            uio.error(str(ex))
+
+
+if __name__ == '__main__':
+    main()
